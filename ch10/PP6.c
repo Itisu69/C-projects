078ff525532051bb36ff946f7d6c0f04344aa6db
Himanshu Patel <hpstudent33@gmail.com>
2026-04-03 17:44:17 +0530

    Fixed an issue

--- ch10/PP6.c
+++ ch10/PP6.c
@@ -1,64 +1,138 @@
-// Reverse Polish Notation (RPN) Calculator
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define MAXSTACK 100
+#define STACK_SIZE 100
 
-double stack[MAXSTACK];
-int sp = 0; // Stack pointer
+void process_char(char i);
 
-void push(double f) {
-    if (sp < MAXSTACK) {
-        stack[sp++] = f;
-    } else {
-        printf("Error: Stack full, can't push %g\n", f);
-    }
+// External vatiables
+char ch;
+int contents[STACK_SIZE], top = 0;
+
+int stack_overflow(void)
+{
+    printf("Expression is too complex\n");
+    exit(EXIT_FAILURE);
 }
 
-double pop() {
-    if (sp > 0) {
-        return stack[--sp];
-    } else {
-        printf("Error: Stack empty\n");
-        return 0.0;
-    }
+int stack_underflow(void)
+{
+    printf("Not enough oprands in expression\n");
+    exit(EXIT_FAILURE);
 }
 
-int main() {
-    char input[100];
-    double op2;
+void make_empty(void)
+{
+    top = 0;
+}
 
-    printf("Enter RPN expression (e.g., '3 4 +'):\n");
-    while (scanf("%s", input) == 1) {
-        if (input[0] >= '0' && input[0] <= '9') {
-            push(atof(input)); // Convert string to double and push
-        } else {
-            switch (input[0]) {
-                case '+':
-                    push(pop() + pop());
-                    break;
-                case '*':
-                    push(pop() * pop());
-                    break;
-                case '-':
-                    op2 = pop();
-                    push(pop() - op2);
-                    break;
-                case '/':
-                    op2 = pop();
-                    if (op2 != 0.0) {
-                        push(pop() / op2);
-                    } else {
-                        printf("Error: Division by zero\n");
-                    }
-                    break;
-                default:
-                    printf("Error: Unknown command %s\n", input);
-            }
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(char i)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = i;
+}
+
+int pop(void)
+{
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+
+int main()
+{
+
+    while (true)
+    {
+        printf("Enter an RPN expression: ");
+
+        while ((ch = getchar()) != EOF && ch != '\n')
+        {
+            process_char(ch);
         }
     }
-
-    printf("Result: %g\n", pop());
     return 0;
+}
+
+void process_char(char i)
+{
+    int operand1, operand2;
+
+    switch (i)
+    {
+    case '0':
+        push(0);
+        break;
+    case '1':
+        push(1);
+        break;
+    case '2':
+        push(2);
+        break;
+    case '3':
+        push(3);
+        break;
+    case '4':
+        push(4);
+        break;
+    case '5':
+        push(5);
+        break;
+    case '6':
+        push(6);
+        break;
+    case '7':
+        push(7);
+        break;
+    case '8':
+        push(8);
+        break;
+    case '9':
+        push(9);
+        break;
+
+    case '+':
+        push(pop() + pop());
+        break;
+
+    case '-':
+        operand2 = pop();
+        operand1 = pop();
+        push(operand1 - operand2);
+        break;
+
+    case '*':
+        push(pop() * pop());
+        break;
+
+    case '/':
+        operand2 = pop();
+        operand1 = pop();
+        push(operand1 / operand2);
+        break;
+
+    case '=':
+        printf("Value of expression: %d\n", pop());
+        break;
+
+    case ' ':
+        break;
+
+    default:
+        exit(EXIT_FAILURE);
+    }
 }
